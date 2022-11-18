a010ab9852e0225ee71c78ed8c8adb9099460f67
Teddy <106557123+TeddyB0MBER@users.noreply.github.com>
2022-11-17 20:31:12 -0800

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json 
 from config import me 
+from mock_data import catalog
 
 
 
@@ -34,5 +35,70 @@ def version():
 @app.get("/api/about")
 def Info():
    return json.dumps(me)
+
+
+@app.get("/api/catalogue")
+def catlog():
+    return json.dumps(catalog)
+
+
+@app.get ("/api/test/count")
+def Amount():
+    return len(catalog)
+
+
+@app.get("/api/catalog/<category>")
+def by_category(category): 
+    # category is to be replaced by a keyword in a URL, so if a keyword in "category" matches with the one placed in the url, it will display the page with the lists.
+    results = []
+    category = category.lower()
+    for product in catalog:
+        if product["category"].lower() == category:
+            results.append(product)
+
+    return json.dumps(results)
+
+
+@app.get ("/api/catalog/search/<text>")
+def by_search(text): 
+    
+    text = text.lower()
+    results = []
+
+    for searchItem in catalog:
+        if text in searchItem["title"].lower() or text in searchItem["category"].lower():
+            results.append(searchItem)
+
+    return json.dumps(results)
+
+@app.get ("/api/categories")
+def cat_list():
+    results = []
+    for listItems in catalog:
+        cat = listItems["category"]
+        if cat not in results:
+            results.append(cat)
+
+    return json.dumps(results)
+
+@app.get ("/api/test/price")
+def price_list():
+    total = 0
+    for value in catalog:
+        total = total + value["price"]
+    
+    return json.dumps(total)
+
+
+
+    #create an endpoint that returns a poduct based on a given _id
+@app.get ("/api/product/<id>")
+def id(id):
+    
+    for identity in catalog:
+        if identity["_id"] == id:
+            return json.dumps(identity)
+
+    return "error: Product not found"
 
 app.run(debug=True)
